e6029ba0591aad583ace1d9fbda09242dbf7c410
AriannaMGonzalez <Jebuschan@live.com>
2022-10-14 23:33:10 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
+import "bootstrap";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function deck() {
+  let suits, numbers, suitsBottom, i;
+  let card = {
+    suits: ["&#9830", "&#9829", "&#9824", "&#9827"],
+    suitsBottom: ["&#9830", "&#9829", "&#9824", "&#9827"],
+    numbers: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  };
+
+  function pullNumber() {
+    let theNumber = Math.floor(Math.random() * card.numbers.length);
+    document.getElementById("randomNumber").innerHTML = card.numbers[theNumber];
+  }
+
+  function pullBothSuits() {
+    let allSuits = Math.floor(Math.random() * card.suits.length);
+    document.getElementById("topSuit").innerHTML = card.suits[allSuits];
+    document.getElementById("bottomSuit").innerHTML = card.suits[allSuits];
+
+    if (
+      card.suits[allSuits] === "&#9830" ||
+      card.suits[allSuits] === "&#9829"
+    ) {
+      document.getElementById("theCard").classList.add("redSuit");
+    } else if (
+      card.suits[allSuits] === "&#9827" ||
+      card.suits[allSuits] === "&#9824"
+    ) {
+      document.getElementById("theCard").classList.remove("redSuit");
+    }
+  }
+
+  window.wholeCard = function() {
+    pullNumber();
+    pullBothSuits();
+  };
 };
